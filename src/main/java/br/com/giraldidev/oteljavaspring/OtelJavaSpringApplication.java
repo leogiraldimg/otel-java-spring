e4170b262bfc87099b26b835b57edabedebf45d1
Leonardo Giraldi Moreno Giuranno <leogiraldimg@gmail.com>
2023-10-27 14:38:50 -0300

    feat: add base project

---
+++ src/main/java/br/com/giraldidev/oteljavaspring/OtelJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.giraldidev.oteljavaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtelJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtelJavaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/giraldidev/oteljavaspring/OtelJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.giraldidev.oteljavaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtelJavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
