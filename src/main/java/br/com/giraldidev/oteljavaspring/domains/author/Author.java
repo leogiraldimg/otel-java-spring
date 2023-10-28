deae9175278473ada22f35de664bf032fec8db70
Leonardo Giraldi Moreno Giuranno <leogiraldimg@gmail.com>
2023-10-28 14:06:55 -0300

    fix(author): remove relationship attribute

--- src/main/java/br/com/giraldidev/oteljavaspring/domains/author/Author.java
+++ src/main/java/br/com/giraldidev/oteljavaspring/domains/author/Author.java
@@ -1,16 +1,10 @@
 package br.com.giraldidev.oteljavaspring.domains.author;
 
-import java.util.Set;
-
-import br.com.giraldidev.oteljavaspring.domains.book.Book;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -35,8 +29,4 @@ public class Author {
     @Column(name = "name")
     @Size(min = 3, max = 100)
     private String name;
-
-    @ManyToMany
-    @JoinTable(name = "tb_book_author", joinColumns = @JoinColumn(name = "author_id"), inverseJoinColumns = @JoinColumn(name = "book_id"))
-    private Set<Book> books;
 }
