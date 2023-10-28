51ce2426f2cfe2230f73cc8b22aff192212dd8e9
Leonardo Giraldi Moreno Giuranno <leogiraldimg@gmail.com>
2023-10-28 12:19:58 -0300

    fix(config): add model mapper bean

---
+++ src/main/java/br/com/giraldidev/oteljavaspring/config/BinariesConfig.java
@@ -0,0 +1,14 @@
+package br.com.giraldidev.oteljavaspring.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BinariesConfig {
+
+    @Bean
+    public ModelMapper getModelMapper() {
+        return new ModelMapper();
+    }
+}
